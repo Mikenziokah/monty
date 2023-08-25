120d4ffec87703b52333cd62351e40a2207bedda
Mikenziokah <mikenziokah@gmail.com>
2023-08-25 12:03:16 -0700

    pop

--- pop.c
+++ pop.c
@@ -9,4 +9,15 @@ void pop(stack_t **stack, unsigned int line_number)
 {
 	(void)stack;
 	(void)line_number;
+
+	stack_t *new = *stack;
+
+	if (head == NULL || head == NULL)
+	{
+		fprint(stderr, "L%u: can't pop on empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	new = (*head)->next;
+	free(*head);
+	*head = new;
 }
