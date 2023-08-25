6863bba98213f6b2685d77c88d7c5f278fe12df5
Mikenziokah <mikenziokah@gmail.com>
2023-08-25 12:10:33 -0700

    nope

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * nop - Doesn’t do anything.
+ * @head: Head of the list
+ * @counter: Number of the line
+ * Return: Void - Nothing
+ **/
+void nop(stack_t **head, unsigned int counter)
+{
+	/* Attribute unused*/
+	(void) head;
+	(void) counter;
+}
